46f63f23a470ed184d8fdb0cd3bd874265bd883f
samarbouzezi <samar.bouzezi@esprit.tn>
2023-05-01 22:18:37 +0100

    photos back

--- src/app/BackOffice/admin-board/admin-board.component.ts
+++ src/app/BackOffice/admin-board/admin-board.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { AuthService } from 'src/app/services/User/auth.service';
+import { StorageService } from 'src/app/services/User/storage.service';
+import { EventBusService } from 'src/app/services/User/_shared/event-bus.service';
 
 @Component({
   selector: 'app-admin-board',
@@ -6,6 +11,58 @@ import { Component } from '@angular/core';
   templateUrl:'./admin-board.component.html',
   styleUrls: ['./admin-board.component.css']
 })
-export class AdminBoardComponent {
+export class AdminBoardComponent {private roles: string[] = [];
+  isLoggedIn = false;
+  showAdminBoard = false;
+  showModeratorBoard = false;
+  username?: string;
+
+  eventBusSub?: Subscription;
+
+  constructor(
+    private storageService: StorageService,
+    private authService: AuthService,
+    private eventBuService: EventBusService,
+    private router:Router
+  ) {}
+
+  ngOnInit(): void {
+    this.isLoggedIn = this.storageService.isLoggedIn();
+
+    if (this.isLoggedIn) {
+      const user = this.storageService.getUser();
+      this.roles = user.roles;
+
+      this.showAdminBoard = this.roles.includes('ROLE_ADMIN');
+      this.showModeratorBoard = this.roles.includes('ROLE_MODERATOR');
+
+      this.username = user.username;
+    }
+
+    this.eventBusSub = this.eventBuService.on('logout', () => {
+      this.logout();
+    });
+  }
+
+  logout(): void {
+    this.authService.logout().subscribe({
+      next: res => {
+        console.log(res);
+        this.storageService.clean();
+
+        window.location.reload();
+      },
+      error: err => {
+        console.log(err);
+      }
+    });
+  }
+
+  doSignout() {
+    this.authService.signout();
+    this.router.navigateByUrl("login");
+  }
+
+
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import {FormsModule} from "@angular/forms";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddAnimalComponent } from './FrontOffice/Animals/add-animal/add-animal.component';
 import { ViewAnimalsComponent } from './FrontOffice/Animals/view-animals/view-animals.component';
 import { UpdateAnimalComponent } from './FrontOffice/Animals/update-animal/update-animal.component';
-import { HeaderComponent } from './FrontOffice/shared/header/header.component';
+
 import { FooterComponent } from './FrontOffice/shared/footer/footer.component';
 import { HomeComponent } from './FrontOffice/home/home.component';
 import { LoginComponent } from './FrontOffice/User/login/login.component';
 import { RegisterComponent } from './FrontOffice/User/register/register.component';
-import {FormsModule} from "@angular/forms";
-import { ProfileComponent } from './FrontOffice/User/profile/profile.component';
-import {NavbarComponent} from "./BackOffice/navbar/navbar.component";
-import {FooterBackEndComponent} from "./BackOffice/footerBackEnd/footer-back-end/footer-back-end.component";
+import { NavbarComponent } from './BackOffice/navbar/navbar.component';
+import { FooterBackEndComponent } from './BackOffice/footerBackEnd/footer-back-end/footer-back-end.component';
 import { AdminBoardComponent } from './BackOffice/admin-board/admin-board.component';
 import { AccountComponent } from './FrontOffice/User/Account/account/account.component';
+import { HeaderComponent } from './FrontOffice/shared/header/header.component';
+import { CreateEventComponent } from './BackOffice/EventBack/create-event/create-event.component';
+
+
 
 @NgModule({
     declarations: [
@@ -30,19 +33,18 @@ import { AccountComponent } from './FrontOffice/User/Account/account/account.com
         HomeComponent,
         LoginComponent,
         RegisterComponent,
-        ProfileComponent,
         NavbarComponent,
       FooterBackEndComponent,
       AdminBoardComponent,
-      AccountComponent
+      AccountComponent,
+      CreateEventComponent,
+      
     ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule,
-
     HttpClientModule,
-
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
